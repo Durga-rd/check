981a3038703338d58e51c7472c614fed4773fb96
Durga-rd <durgarasasundaram@gmail.com>
2025-07-25 13:17:42 +0530

    changes like gitignore

--- routes/resume.js
+++ routes/resume.js
@@ -11,7 +11,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   ssl: { rejectUnauthorized: false },
 });
